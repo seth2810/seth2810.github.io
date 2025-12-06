acbee15f88572d8704b9a2b74a418bf9248f720f
Roman Gafurov <gafurov.roma89@gmail.com>
2025-12-06 22:21:04 +0400

    fixup! chore(dev): plug tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,6 @@ const Index = () => (
       <h1>Roman Gafurov</h1>
       <FontAwesomeIcon icon={faInfinity} width={22} />
       <p>Software Engineer</p>
-
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <Button>Save changes</Button>
     </main>
 
     <footer>
